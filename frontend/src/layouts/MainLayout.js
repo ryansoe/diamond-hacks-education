18fd3df8da91ca3379429f68be3c2206f0cb74f5
rohanmad <rohanmadan2027@gmail.com>
2025-04-05 14:31:26 -0700

    changed color sidebar

--- frontend/src/layouts/MainLayout.js
+++ frontend/src/layouts/MainLayout.js
@@ -78,11 +78,11 @@ const MainLayout = () => {
       <div className="hidden md:flex md:flex-shrink-0">
         <div className="flex flex-col w-64">
           <div className="flex flex-col h-0 flex-1">
-            <div className="flex items-center h-16 flex-shrink-0 px-4 bg-primary-700">
-              <h1 className="text-white text-xl font-bold">Deadline Tracker</h1>
+            <div className="flex items-center h-16 flex-shrink-0 px-4 " style={{ backgroundColor: "#7c3aed" }}>
+              <h1 className="text-white text-xl font-bold" >Deadline Tracker</h1>
             </div>
             <div className="flex-1 flex flex-col overflow-y-auto">
-              <nav className="flex-1 px-2 py-4 bg-primary-700 space-y-1">
+              <nav className="flex-1 px-2 py-4 space-y-1" style={{ backgroundColor: "#7c3aed" }}>
                 {navigation.map((item) => (
                   <NavLink
                     key={item.name}
